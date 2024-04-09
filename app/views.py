d072d01e123e0f0f732f29cb6e53e1f4cddf7f46
aditya kumar <adityakr2019@gmail.com>
2024-04-09 13:27:53 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,44 +1,74 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Student
+from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
+    # get(coloumn_name=value)
+    # user = Student.objects.get(id=2) # get arguments always used primary-key coloumn
+    # user = Student.objects.get(stu_name="aditya Kumar")
+    # user = Student.objects.get(stu_name="Arvind singh")
+    # print(user.id, user.stu_name,user.stu_city)
+    # return HttpResponse(user)
     
-     # latest(*fields/Coloumn)
-    user = Student.objects.latest("id") # latest entry in Student table
-    print(user.id, user.stu_name,user.stu_city)
-    return HttpResponse(user)
+    ## first()
+    # user = Student.objects.first()
+    # # user = Student.objects.order_by('stu_name').first()
+    # # user = Student.objects.order_by('-stu_name').first()
+    # print(user.id, user.stu_name,user.stu_city)
+    # return HttpResponse(user)
 
-    # earliest(*fields/Coloumn)
-#     user = Student.objects.earliest("id") # very first entry in this Student table
-#     print(user.id, user.stu_name,user.stu_city)
-#     return HttpResponse(user)
+    # last()
+    # user = Student.objects.last()
+    # # user = Student.objects.order_by('stu_name').last()
+    # # user = Student.objects.order_by('-stu_name').last()
+    # print(user.id, user.stu_name,user.stu_city)
+    # return HttpResponse(user)
 
-#     # exists()
-#     user = Student.objects.all()
-#     print(user.exists())
-#     return HttpResponse(user)
+    # # latest(*fields/Coloumn)
+    # user = Student.objects.latest("id") # latest entry in Student table
+    # print(user.id, user.stu_name,user.stu_city)
+    # return HttpResponse(user)
 
-#   # update(**kwargs)
-#     user = Student.objects.filter(id=1).update(stu_name='Ravi Thakur',stu_city='Indore')
-#     print(user)
-#     return HttpResponse(user)
+    # # earliest(*fields/Coloumn)
+    # user = Student.objects.earliest("id") # very first entry in this Student table
+    # print(user.id, user.stu_name,user.stu_city)
+    # return HttpResponse(user)
 
+    # # exists()
+    # user = Student.objects.all()
+    # print(user.exists())
+    # return HttpResponse(user)
 
-## Update Multiple objects values at a time
-    # user = Student.objects.filter(stu_city="Bhopal").update(stu_name='Neeraj')
+    # # create(coloumn1=value1,coloumn2=value2)
+    # user = Student.objects.create(stu_name='Ravi',stu_email='ravi@gmail.com',stu_city='Bhopal')
+    # print(user.id, user.stu_name,user.stu_city)
+    # return HttpResponse(user)
+
+    # # get_or_create(default=None,**kwargs)
+    # user,created = Student.objects.get_or_create(stu_name='RaviSingh',stu_email='ravisingh@gmail.com',stu_city='Pune')
+    # print(user.id, user.stu_name,user.stu_city)
+    # print(created)
+    # return HttpResponse(user)
+
+    ## update(**kwargs)
+    # user = Student.objects.filter(id=1).update(stu_name='Ravi Thakur',stu_city='Indore')
+    # print(user)
+    # return HttpResponse(user)
+    
+    ## Update Multiple objects values at a time
+    # user = Student.objects.filter(stu_city="Bhopal").update(stu_name='aditya')
     # print(user)
     # return HttpResponse(user)
 
     ## Delete the given object
-    # user = Student.objects.get(id=14).delete() or
-    # user = Student.objects.get(id=14)
+    user = Student.objects.get(id=1).delete()
+    # user = Student.objects.get(id=1)
     # user.delete()
-    # user = Student.objects.filter(stu_name = "Neeraj").delete()
+    # user = Student.objects.filter(stu_name = "aditya").delete()
     # print(user)
-    # return HttpResponse(user)
+    return HttpResponse(user)
 
     ## count all no of object prasent in table
     # user = Student.objects.all()
@@ -47,107 +77,8 @@ def home(request):
     # user = Student.objects.explain()
     # print(user)
 
-
-#     last()
-#     user = Student.objects.last()
-#     # user = Student.objects.order_by('stu_name').last()
-#     # user = Student.objects.order_by('-stu_name').last()
-#     print(user.id, user.stu_name,user.stu_city)
-#     return HttpResponse(user)
-
-
-     # first()
-#     user = Student.objects.first()
-#     # user = Student.objects.order_by('stu_name').first()
-#     # user = Student.objects.order_by('-stu_name').first()
-#     print(user.id, user.stu_name,user.stu_city)
-#     return HttpResponse(user)
-
-
-
-
-
-
-
-        # user = Student.objects.create(stu_name='ajay',
-        #                               stu_email='ajay@gmail.com',
-        #                               stu_contact='4561237',
-        #                               stu_city='chakhaji')
-        # # print("data cretat")
-        # return HttpResponse(user)
-
-        # user=Student.objects.first()
-        # return HttpResponse(user)
-
-        # user=Student.objects.get(id=5)
-        # print(user.id,user.stu_name,user.stu_email,user.stu_contact,user.stu_city)
-        
-        # return HttpResponse(user)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-    
-
-        # user = Student.objects.get(id=5)
-
-        # print(user.id,user.stu_name,user.stu_email,user.stu_contact,user.stu_city)
-        # user = Student.objects.get(stu_city = 'bhopal')
-        # user = Student.objects.get(stu_city = 'bihar')
-        # user = Student.objects.order_by('-stu_name').first()
-
-        # return HttpResponse(data)
-
-        # user = Student.objects.get(stu_city = 'bhopal')
-        # user = Student.objects.get(stu_city = 'bihar')
-        # user = Student.objects.order_by('-stu_name').first()
-
-        # return HttpResponse(user)
-
-
-      
-    
+    # # update_or_create(**kwargs,default=None)
+    # user,created = Student.objects.update_or_create(id=15, stu_city="Pune", defaults={'stu_name':"Ravi"})
+    # print(user)
+    # print(created)
+    # return HttpResponse(user)
